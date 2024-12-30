acdeaa499e17e2a5bc00bf84459e468177cc1a18
atul074 <atultandan074@gmail.com>
2024-12-30 12:08:48 +0530

    order slice

--- client/vite-project/src/store/Store.jsx
+++ client/vite-project/src/store/Store.jsx
@@ -5,6 +5,7 @@ import adminProductsSlice from "./admin/products-slice";
 import shopProductsSlice from "./shop/products-slice";
 import shopCartSlice from "./shop/cart-slice";
 import shopAddressSlice from "./shop/address-slice";
+import shopOrderSlice from "./shop/order-slice";
 const Store = configureStore({
   reducer: {
     auth: authReducer, 
@@ -13,6 +14,8 @@ const Store = configureStore({
     shopProducts: shopProductsSlice,
     shopCart: shopCartSlice,
     shopAddress: shopAddressSlice,
+    shopOrder: shopOrderSlice,
+
   },
 });
 
