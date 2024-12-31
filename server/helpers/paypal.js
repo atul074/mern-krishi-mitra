c8baa46523edc0935891c8ab8540c2d5540d3cba
atul074 <atultandan074@gmail.com>
2024-12-31 20:34:47 +0530

    resolve paypal integration

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -2,8 +2,7 @@ const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
   mode: "sandbox",
-  client_id: "AVetvBXvH4LB2jod7Mmj_2bwjM0kLrbLR3ptvS6PoOhUepnAjo8696APjWm3cFQJclJ3ckEkCUjJx3Hu",
-  client_secret: "EEutEAn8xrjXlhOBmebOFaVH99aNNvOaFkX3C4nKl2BdpuUmcIuBOG_F8SbuNks4vwv8LtLaKS4UX6JE",
+  client_id: "AaDD2atOGsm2xKfuk36t4BQl4BU55iOfPrSiczAev-xUrbx7QvdFxPDrNtVj7QFTrP4jIBcnBmfDpvl_",
+  client_secret: "EHhFNYYx540WYUUN77HVtsdRqakj9vwVjjdfBxsz37_2Co3YuXt-SvdyP_pn1t2-BWyuC-TVKzKjSwWn",
 });
-
 module.exports = paypal;
