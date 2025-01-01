cb514c66a75d924685bfbbbe81d9350a49037d9c
atul074 <atultandan074@gmail.com>
2025-01-01 20:26:27 +0530

    remove unneccesary package

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -1,5 +1,5 @@
-const cloudinary = require("cloudinary").v2;
-const multer = require("multer");
+// const cloudinary = require("cloudinary").v2;
+// const multer = require("multer");
 
 // cloudinary.config({
 //   cloud_name: "de7imsn1h",
