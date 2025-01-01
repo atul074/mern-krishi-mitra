1125f6cb6cc8da9fe6b2a401f9f47bbc1a383193
atul074 <atultandan074@gmail.com>
2025-01-01 11:13:54 +0530

    review backend

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const shopCartRouter = require("./routes/shop/cart-routes");
 const shopAddressRouter = require("./routes/shop/address-routes");
 const shopOrderRouter = require("./routes/shop/order-routes");
 const shopSearchRouter = require("./routes/shop/search-routes");
+const shopReviewRouter = require("./routes/shop/review-routes");
 mongoose
   .connect("mongodb://localhost:27017/mern-krishi-mitra")
   .then(() => console.log("MongoDB connected"))
@@ -46,5 +47,6 @@ app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/address", shopAddressRouter); 
 app.use("/api/shop/order", shopOrderRouter);
 app.use("/api/shop/search", shopSearchRouter);
+app.use("/api/shop/review", shopReviewRouter);
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
