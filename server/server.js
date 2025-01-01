8f2caaddcd5be44368ab823e403ace0b884131bb
atul074 <atultandan074@gmail.com>
2025-01-01 20:26:43 +0530

    env setup

--- server/server.js
+++ server/server.js
@@ -5,24 +5,26 @@ const cors = require("cors");
 const authRouter = require("./routes/auth/Auth-route");
 const adminProductsRouter = require("./routes/admin/product-routes");
 const adminOrderRouter = require("./routes/admin/order-routes");
-
+require('dotenv').config();
+  
 const shopProductsRouter = require("./routes/shop/products-routes");
 const shopCartRouter = require("./routes/shop/cart-routes");
 const shopAddressRouter = require("./routes/shop/address-routes");
 const shopOrderRouter = require("./routes/shop/order-routes");
 const shopSearchRouter = require("./routes/shop/search-routes");
 const shopReviewRouter = require("./routes/shop/review-routes");
+
+
 mongoose
-  .connect("mongodb://localhost:27017/mern-krishi-mitra")
+  .connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(error));
 
 const app = express();
-const PORT = process.env.PORT || 8000;
-
+const PORT = process.env.PORT  ;
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
